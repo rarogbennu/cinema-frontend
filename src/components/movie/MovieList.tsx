a59b7b0f0e1b525a754437697fa48774ff4ad001
svdf18 <simonvandeurs@hotmail.com>
2024-03-18 17:38:35 +0100

    jeg er rigtig dårlig til at huske og skifte branch
    det må i meget undskylde!

--- src/components/movie/MovieList.tsx
+++ src/components/movie/MovieList.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "./MoviesLayout.css";
 import { useEffect, useState } from "react";
-import { getMovies, Movie as APIMovie } from "../../services/apiFacade";
+import { getAllMovies, Movie as APIMovie } from "../../services/apiFacade";
 
 export default function MovieList() {
   const [movies, setMovies] = useState<Array<APIMovie>>([]);
@@ -10,7 +10,7 @@ export default function MovieList() {
   useEffect(() => {
     const fetchMovies = async () => { 
       try {
-        const movies = await getMovies(); 
+        const movies = await getAllMovies(); 
         setMovies(movies);
       } catch (error) {
         setError("Error loading movies, is the server running?");
--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -1,8 +1,14 @@
 import { API_URL } from "../settings";
 import { makeOptions, handleHttpErrors } from "./fetchUtils";
 
-const MOVIE_URL = API_URL + "/movies";
 const CINEMA_URL = API_URL + "/cinemas";
+const SCREEN_URL = API_URL + "/screens";
+const SEAT_URL = API_URL + "/seats";
+const SCREENING_URL = API_URL + "/screenings";
+const MOVIE_URL = API_URL + "/movies";
+const RESERVATION_URL = API_URL + "/reservations";
+const TOTAL_RESERVATION_URL = API_URL + "/total-reservations";
+
 
 interface Cinema {
   id: number | null;
@@ -11,53 +17,262 @@ interface Cinema {
   screens: string;
 }
 
-interface Movie {
-  id: number | null;
+interface Screen {
+  id: number;
   name: string;
-  duration: string;
-  description: string;
+  capacity: number;
+  rows: number;
+  created?: string;
+  edited?: string;
+}
+
+interface Seat {
+  id: number;
+  screenId: number;
+  priceCategoryId: number;
+}
+
+interface Screening {
+  id: number;
+  movieId: number;
+  screenId: number;
+  startTime: string;
+  endTime: string;
+}
+
+interface Movie {
+  title: string;
+  year: string;
+  rated: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  language: string;
+  country: string;
+  awards: string;
   poster: string;
-  starringActors: string;
-  instructor: string;
-}
-let cinemas: Cinema[] = [];
-let movies: Array<Movie> = [];
-
-async function getMovies(): Promise<Array<Movie>> {
-  if (movies.length > 0) return [...movies];
-  console.log("fetching movies");
-  return fetch(MOVIE_URL).then(handleHttpErrors);
-}
-
-async function getMovie(id: number): Promise<Movie> {
-  //@ts-ignore
-  if (movies.length > 0) return [...movies];
-  return fetch(MOVIE_URL + "/" + id).then(handleHttpErrors);
+  ratings: Rating[];
+  metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  type: string;
+  dvd: string;
+  boxOffice: string;
+  production: string;
+  website: string;
+  response: string;
 }
 
-async function addMovie(newMovie: Movie): Promise<Movie> {
-  const method = newMovie.id ? "PUT" : "POST";
-  const options = makeOptions(method, newMovie);
-  const URL = newMovie.id ? `${MOVIE_URL}/${newMovie.id}` : MOVIE_URL;
-  return fetch(URL, options).then(handleHttpErrors);
+interface Rating {
+  source: string;
+  value: string;
 }
 
-async function deleteMovie(id: number): Promise<Movie> {
-  const options = makeOptions("DELETE", null);
-  return fetch(`${MOVIE_URL}/${id}`, options).then(handleHttpErrors);
+interface Reservation {
+  id: number;
+  screeningId: number;
+  seatId: number;
+  totalReservationId: number;
+  dummyUser: string;
+  created?: string;
+  edited?: string;
+}
+
+interface TotalReservation {
+  id: number;
+  created?: string;
+  edited?: string;
+}
+
+let cinema: Cinema[] = [];
+let screen: Screen[] = [];
+let seat: Seat[] = [];
+let screening: Screening[] = [];
+let movie: Movie[] = [];
+let reservation: Reservation[] = [];
+let totalReservation: TotalReservation[] = [];
+
+// Get Cinemas
+async function getAllCinemas(): Promise<Cinema[]> {
+  if (cinema.length > 0) return [...cinema];
+  const res = await fetch(CINEMA_URL).then(handleHttpErrors);
+  cinema = [...res];
+  return cinema;
 }
 
 async function getCinema(id: number): Promise<Cinema> {
   return fetch(CINEMA_URL + "/" + id).then(handleHttpErrors);
 }
 
-async function getAllCinemas(): Promise<Cinema[]> {
-  if (cinemas.length > 0) return [...cinemas];
-  const res = await fetch(CINEMA_URL).then(handleHttpErrors);
-  cinemas = [...res];
-  return cinemas;
+// Get Screens
+async function getAllScreens(): Promise<Screen[]> {
+  if (screen.length > 0) return [...screen];
+  const res = await fetch(SCREEN_URL).then(handleHttpErrors);
+  screen = [...res];
+  return screen;
 }
 
-export type { Movie, Cinema };
+async function getScreen(id: number): Promise<Screen> {
+  return fetch(SCREEN_URL + "/" + id).then(handleHttpErrors);
+}
+
+// Get All Seats
+async function getAllSeats(): Promise<Seat[]> {
+  if (seat.length > 0) return [...seat];
+  const res = await fetch(SEAT_URL).then(handleHttpErrors);
+  seat = [...res];
+  return seat;
+}
+
+async function getSeat(id: number): Promise<Seat> {
+  return fetch(SEAT_URL + "/" + id).then(handleHttpErrors);
+}
+
+// Get All Screenings
+async function getAllScreenings(): Promise<Screening[]> {
+  if (screening.length > 0) return [...screening];
+  const res = await fetch(SCREENING_URL).then(handleHttpErrors);
+  screening = [...res];
+  return screening;
+}
+
+async function getScreening(id: number): Promise<Screening> {
+  return fetch(SCREENING_URL + "/" + id).then(handleHttpErrors);
+}
+
+// Get Reservations
+async function getAllReservations(): Promise<Reservation[]> {
+  if (reservation.length > 0) return [...reservation];
+  const res = await fetch(RESERVATION_URL).then(handleHttpErrors);
+  reservation = [...res];
+  return reservation;
+}
+
+async function getReservation(id: number): Promise<Reservation> {
+  return fetch(RESERVATION_URL + "/" + id).then(handleHttpErrors);
+}
+
+// Create Reservation
+async function createReservation(newReservation: Reservation): Promise<Reservation> {
+  const options = makeOptions("POST", newReservation, true); 
+  return fetch(RESERVATION_URL, options).then(handleHttpErrors);
+}
+
+// Delete Reservation
+async function deleteReservation(id: number): Promise<void> {
+  const options = makeOptions("DELETE", null, true);
+  await fetch(`${RESERVATION_URL}/${id}`, options).then(handleHttpErrors);
+  console.log("Reservation with id: " + id + " has been deleted");
+}
+
+// Get Total Reservations
+async function getAllTotalReservations(): Promise<TotalReservation[]> {
+  if (totalReservation.length > 0) return [...totalReservation];
+  const res = await fetch(TOTAL_RESERVATION_URL).then(handleHttpErrors);
+  totalReservation = [...res];
+  return totalReservation;
+}
+
+async function getTotalReservation(id: number): Promise<TotalReservation> {
+  return fetch(TOTAL_RESERVATION_URL + "/" + id).then(handleHttpErrors);
+}
+
+// Create Total Reservation 
+async function createTotalReservation(newTotalReservation: TotalReservation): Promise<TotalReservation> {
+  const options = makeOptions("POST", newTotalReservation, true); 
+  return fetch(TOTAL_RESERVATION_URL, options).then(handleHttpErrors);
+}
+
+// Delete Total Reservation 
+async function deleteTotalReservation(id: number): Promise<void> {
+  const options = makeOptions("DELETE", null, true);
+  await fetch(`${TOTAL_RESERVATION_URL}/${id}`, options).then(handleHttpErrors);
+  console.log("TotalReservation with id: " + id + " has been deleted");
+}
+
+
+// Get Movies
+async function getAllMovies(): Promise<Array<Movie>> {
+  if (movie.length > 0) return [...movie];
+  const res = await fetch(MOVIE_URL).then(handleHttpErrors);
+  movie = [...res];
+  return movie;
+}
+
+async function getMovie(imdbID: string): Promise<Movie> {
+  //@ts-ignore
+  return fetch(MOVIE_URL + "/" + imdbID).then(handleHttpErrors);
+}
+
+// Add Movie
+async function addMovie(newMovie: Movie): Promise<Movie> {
+  const method = newMovie.imdbID ? "PUT" : "POST";
+  const options = makeOptions(method, newMovie);
+  const URL = newMovie.imdbID ? `${MOVIE_URL}/${newMovie.imdbID}` : MOVIE_URL;
+  return fetch(URL, options).then(handleHttpErrors);
+}
+
+// Delete Movie
+async function deleteMovie(imdbID: string): Promise<Movie> {
+  const options = makeOptions("DELETE", null);
+  return fetch(`${MOVIE_URL}/${imdbID}`, options).then(handleHttpErrors);
+}
+
+// Get All Data
+async function getAllData(): Promise<void> {
+  try {
+    const cinemas = await getAllCinemas();
+    console.log("Cinemas:", cinemas);
+
+    const screens = await getAllScreens();
+    console.log("Screens:", screens);
+
+    const seats = await getAllSeats();
+    console.log("Seats:", seats);
+
+    const screenings = await getAllScreenings();
+    console.log("Screenings:", screenings);
+
+    const reservations = await getAllReservations();
+    console.log("Reservations:", reservations);
+
+    const totalReservations = await getAllTotalReservations();
+    console.log("Total Reservations:", totalReservations);
+
+    const movies = await getAllMovies();
+    console.log("Movies:", movies);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+getAllData();
+
+export type { Movie, Cinema, Screen, Seat, Screening, Reservation};
 // eslint-disable-next-line react-refresh/only-export-components
-export { getMovies, getMovie, addMovie, deleteMovie, getAllCinemas, getCinema };
+export {  getAllCinemas,
+          getCinema, 
+          getAllScreens, 
+          getScreen,
+          getAllSeats,
+          getSeat,
+          getAllScreenings,
+          getScreening,
+          getAllReservations,
+          getReservation,
+          createReservation,
+          deleteReservation,
+          getAllTotalReservations,
+          getTotalReservation, 
+          createTotalReservation,
+          deleteTotalReservation, 
+          getAllMovies,
+          getMovie,
+          addMovie, 
+          deleteMovie
+        };

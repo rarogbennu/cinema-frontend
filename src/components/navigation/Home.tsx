ea35ac154c6a8c0562198cb907c2cbb408c3ab52
Tunafan <124045808+Tunafan@users.noreply.github.com>
2024-03-19 09:27:32 +0100

    UI design
    KINO-46 tema

--- src/components/navigation/Home.tsx
+++ src/components/navigation/Home.tsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+
   return (
     <>
-      <h1>Velkommen til kino</h1>
-      <button className="card">Roskilde Bio</button>
-      <button className="card">Kbh Bio</button>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1>Velkommen til ROLF* Kino</h1>
+      <p>*rolling on laughing floors</p>
+      <div className="cinemas-home-container">
+        <Link to="/cinema/roskilde" className="card-button">
+          Roskilde Bio
+        </Link>
+        <Link to="/cinema/kbh" className="card-button">
+          KBH Bio
+        </Link>
+      </div>
+      <div className="button-container">
+        <button
+          className="button-of-joy"
+          onClick={() => setCount((count) => count + 1)}
+        >
           Klik HER, hvis du havde en god oplevelse❤️!!!!
         </button>
         <p>Gode oplevelser: {count} </p>

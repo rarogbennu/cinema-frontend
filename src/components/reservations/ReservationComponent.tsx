f8ee9227023b32385456e4b66f21d541990dd75e
svdf18 <simonvandeurs@hotmail.com>
2024-03-21 08:48:23 +0100

    reservation comp continue
    KINO-67 Reservation component

--- src/components/reservations/ReservationComponent.tsx
+++ src/components/reservations/ReservationComponent.tsx
@@ -11,6 +11,7 @@ export default function ReservationComponent() {
   const [screen, setScreen] = useState<ApiScreen | null>(null);
   const [seats, setSeats] = useState<ApiSeat[]>([]);
   const [reservations, setReservations] = useState<ApiReservation[]>([]);
+  const [selectedSeats, setSelectedSeats] = useState<number[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +37,19 @@ export default function ReservationComponent() {
     fetchData();
   }, [id]);
 
+  const isSeatReserved = (seatId: number) => {
+      return reservations.some((reservation) => reservation.seatId === seatId);
+  };
+
+  const handleSeatClick = (seatId: number) => {
+    if (selectedSeats.includes(seatId)) {
+      setSelectedSeats(selectedSeats.filter((id) => id !== seatId));
+    } else {
+      setSelectedSeats([...selectedSeats, seatId]);
+    }
+    console.log(`Seat ${seatId} clicked`);
+  };
+
   return (
     <>
       <h3>Screening Details</h3>
@@ -44,22 +58,16 @@ export default function ReservationComponent() {
       {screen && <p>Rows: {screen.rows}</p>}
       {screen && <p>Capacity: {screen.capacity}</p>}
 
-      <h3>Reservations</h3>
-        {reservations.map((reservation) => (
-          <li key={reservation.id}>
-            <p>Reservation ID: {reservation.id}</p>
-            <p>Seat ID: {reservation.seatId}</p>
-            <p>Customer Name: {reservation.dummyUser}</p>
-          </li>
-        ))}
-
-      <h3>Seats</h3>
       <div className="screen"><p>Screen</p></div>
       {screen && (
         <div className="seats-grid" style={{ gridTemplateColumns: `repeat(${Math.ceil(screen.capacity / screen.rows)}, 1fr)` }}>
           {seats.map((seat) => (
-            <div key={seat.id} className="seat">
-              <p>Seat</p>{seat.id}
+            <div
+              key={seat.id}
+              className={`seat ${isSeatReserved(seat.id) ? 'reserved' : ''} ${selectedSeats.includes(seat.id) ? 'selected' : ''}`}
+              onClick={isSeatReserved(seat.id) ? undefined : () => handleSeatClick(seat.id)}
+              style={{ cursor: isSeatReserved(seat.id) ? 'default' : 'pointer' }}
+            >
             </div>
           ))}
         </div>

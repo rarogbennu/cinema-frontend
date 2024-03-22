237fbd51fc2c5417bb60dd914ae99d92ed6dc25f
svdf18 <simonvandeurs@hotmail.com>
2024-03-22 08:28:21 +0100

    make reservation

--- src/components/totalReservations/TotalReservationComponent.tsx
+++ src/components/totalReservations/TotalReservationComponent.tsx
@@ -2,7 +2,6 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { useAuth } from "../../security/AuthProvider";
 import { createReservation } from "../../services/apiFacade";
-import "./TotalReservationLayout.css";
 
 export default function TotalReservationComponent() {
   const location = useLocation();
